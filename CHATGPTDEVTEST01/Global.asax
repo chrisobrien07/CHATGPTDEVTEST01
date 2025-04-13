793525cdf3d27ff9e163a40e6f1f7d6ae1216ab8
christopher obrien <chris.obrien.07@gmail.com>
2025-04-13 23:23:01 +0100

    Add project files.

---
+++ CHATGPTDEVTEST01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CHATGPTDEVTEST01.MvcApplication" Language="C#" %>
---
+++ CHATGPTDEVTEST01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CHATGPTDEVTEST01.Startup))]
+namespace CHATGPTDEVTEST01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
